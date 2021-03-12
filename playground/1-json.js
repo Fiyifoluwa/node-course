6739e65ddc20bdd4c2cf9f41ebce945ff0851e64
fiyifoluwa <fiyifoluwa@outlook.com>
2021-03-12 10:53:06 +0100

    update

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,13 +1,15 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const dataBuffer = fs.readFileSync('1-json.json');
+const dataBuffer = fs.readFileSync("1-json.json");
 const dataJSON = dataBuffer.toString();
 const user = JSON.parse(dataJSON);
 
-user.name = 'Fiyifoluwa';
+user.name = "Fiyifoluwa";
 user.age = 23;
 
 const userJSON = JSON.stringify(user);
-fs.writeFileSync('1-json.json', userJSON);
+fs.writeFileSync("1-json.json", userJSON);
 
 console.log(user);
+
+// read file, convert to a string, parse that data, input values, pass that data in with json.stringify, write file
